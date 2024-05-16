01cf09484567d81bebbfe7c7f27d9a8898c2750e
HLizhm <hamlizama@gmail.com>
2024-05-15 20:57:31 -0600

    El admin y contraseña vulnerables, usar algun tipo de cifrado al finalizar

--- public/index.php
+++ public/index.php
@@ -8,7 +8,25 @@
 </head>
 <body>
 
+    <div class="formulario">
+        <h1>Inicio de sesión</h1>
+        <form method="post">
+            <div class="username">
+                <input type="text" required>
+                <label>Nombre de usuario</label>
+            </div>
+            <div class="username">
+                <input type="password" required>
+                <label>Contraseña</label>
+            </div>
+            <div class="recordar">¿Olvidó su contraseña?</div>
+            <input type="submit" value="Iniciar">
+            <div class="registrarse">
+                <a href="#">Registrarse aquí</a>
+            </div>
+        </form>
 
+    </div>
     
 </body>
 </html>
--- public/iniciar sesion.php
+++ public/iniciar sesion.php
@@ -1,6 +1,6 @@
 <?php
-
-    include('conexion.php');
+session_start();
+include('conexion.php');
 
 if (isset($_POST['username']) && isset($_POST['password'])){
 
@@ -8,21 +8,32 @@ if (isset($_POST['username']) && isset($_POST['password'])){
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
-        $datadata = htmlspecialchars_decode($data);
         return $data;
     }
 
+    $user = validate($_POST['username']); 
+    $password = validate($_POST['password']);
 
-
-
-
-    $usuario = validate($_POST($usuario)); 
-    $contrasena = validate(($_POST($contrasena)));
-    if (empty($usuario)){
-        header("Location: Index.php?error = El Usuario Es Requerido");
+    if (empty($user)){
+        header("Location: Index.php?error=El Usuario Es Requerido");
         exit();
-    }elseif (empty($usuario)){
-        header("Location: Index.php?error = El Usuario Es Requerido");
+    } elseif (empty($password)){
+        header("Location: Index.php?error=La Contraseña Es Requerida");
         exit();
+    }else{
+
+        $admin = "moleconpollo1234";
+        $admin_password = "franciscomememe123";
+
+        if ($user === $admin && $password === $admin_password){
+            $_SESSION['user'] = $admin;
+            $_SESSION['password'] = $password;
+            header("Location: dashboard.php");
+            exit();
+        } else {
+            header("Location: Index.php?error=Usuario o Contraseña incorrectos");
+            exit();
+        }
+    }
 }
-}
+?>
